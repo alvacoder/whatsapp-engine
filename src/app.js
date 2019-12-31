ceb5fee2349c17b040afa2264d63cfb37ee8a76a
realprinceviper <idrisadeniji01@gmail.com>
2019-12-31 09:31:10 +0100

    send one way whatsapp message to recipient

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const twilio = require('twilio')
+const client = new twilio(accountSid, authToken)
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use
+
+app.post('/', (req, res)=> {
+    const from = 'whatsapp:+14155238886'
+    const { body, recipient } = req.body
+    //console.log(req.body);
+    const payload = {
+        body,
+        from,
+        to: `whatsapp:${recipient}`
+    }
+
+    const sendMsg = client.messages.create(payload)
+    sendMsg.then((response)=> {
+        res.send(response)
+    }).done()
+})
 
 
 app.listen(port, ()=> {
