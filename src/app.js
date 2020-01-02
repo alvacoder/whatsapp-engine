2db5db0a3fb73c3516983efc2651d875db2e3ba0
realprinceviper <idrisadeniji01@gmail.com>
2020-01-02 05:25:06 +0100

    Fetch all chat logs by date

--- src/app.js
+++ src/app.js
@@ -13,10 +13,9 @@ const client = new twilio(accountSid, authToken)
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use
 
-// Send message to whatsapp user
-app.post('/send', (req, res)=> {
+// Send message
+app.post('/send', (req, res) => {
     const from = 'whatsapp:+14155238886'
     const { body, recipient } = req.body
     //console.log(req.body);
@@ -33,9 +32,23 @@ app.post('/send', (req, res)=> {
     }).done()
 })
 
-app.get('/', (req, res)=> {
-    res.send('hello whatsapp')
+//Chat Logs by Date && Paginate
+app.get('/logs', async(req, res) => {
+    try {
+        let msgHistory = await client.messages.list({limit: 5})
+        res.send(msgHistory)
+    } catch (error) {
+        res.status(400).send
+    }
 })
+
+/* app.get('/logs', (req, res) => {
+    let msgHistory = client.messages.list({limit: 5})
+    msgHistory.then((data)=> {
+        res.send(msgHistory)
+    })
+}) */
+
 
 
 app.listen(port, ()=> {
