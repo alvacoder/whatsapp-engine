6bce5c53f1a248a07c65ad4e7514040ba6abf9a2
realprinceviper <idrisadeniji01@gmail.com>
2020-01-08 16:40:51 +0100

    change whatsapp api to messagebird

--- src/app.js
+++ src/app.js
@@ -7,62 +7,78 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const statusCallback = process.env.TWILIO_STATUS_CALLBACK
 const replyURL = process.env.TWILIO_REPLY_URL
+const accessKey = process.env.MESSAGEBIRD_ACCESS_KEY,
+    channelId = process.env.MESSAGEBIRD_CHANNEL_ID,
+    reportUrl = process.env.MESSAGEBIRD_REPORT_URL
 
-const twilio = require('twilio')
-const client = new twilio(accountSid, authToken)
-const port = process.env.PORT || 3000
+
+const messagebird = require('messagebird')(accessKey, null, ["ENABLE_CONVERSATIONSAPI_WHATSAPP_SANDBOX"])
+const port = process.env.PORT || 3800
 
 app.use(express.json())
 
-// Send message
-app.post('/send', (req, res) => {
-    const from = 'whatsapp:+14155238886'
-    const { body, recipient } = req.body
-    //console.log(req.body);
-    const payload = {
-        body,
-        from,
-        statusCallback,
-        to: `whatsapp:${recipient}`
+//get messagebird credit balance
+let balance = messagebird.balance.read(function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  });
+
+// start a conversation
+app.post('/start', (req, res)=> {
+    let payload = {
+        'to': '2347085183282',
+        channelId,
+        'type': 'hsm',
+        'content': {
+                'hsm': {
+                'namespace': 'eb4d5655_295e_4ada_8614_702a0efe2c85',
+                'templateName': 'verification',
+                'language': {
+                    'policy': 'deterministic',
+                    'code': 'en'
+                },
+                'params': [
+                    {"default": "MessageBird"},
+                    {"default": "123456"},
+                ]
+                }
+            }
+        }
+    let startConv = async () => {
+        let data = await messagebird.conversations.start(payload)
+        return data;
     }
 
-    const sendMsg = client.messages.create(payload)
-    sendMsg.then((response)=> {
-        res.send(response)
-    }).done()
-})
-
-//Chat Logs by Date && Paginate
-app.get('/logs', async(req, res) => {
-    try {
-        let msgHistory = await client.messages.list({limit: 5})
-        res.send(msgHistory)
-    } catch (error) {
-        res.status(400).send
-    }
-})
-
-/* app.get('/logs', (req, res) => {
-    let msgHistory = client.messages.list({limit: 5})
-    msgHistory.then((data)=> {
-        res.send(msgHistory)
+    startConv().then((response)=> {
+        console.log(response)
+        res.status(201)
+    }).catch((err)=> {
+        console.log(err)
+        res.status(400)
     })
-}) */
-
-//Chat logs by recipient
-app.get('/chat/:recipient', async(req, res)=> {
-    const to = req.params.recipient
-
-    try {
-        let msgHistory = await client.messages.list({
-            to,
-            limit: 5
-        })
-        res.send(msgHistory)
-    } catch (error) {
-        res.status(400).send();
-    }
 })
+
+// Send message
+app.post('/send', (req, res)=> {
+    var params = {
+    'to': '+2347085183282',
+    channelId,
+    'type': 'text',
+    'content': { 'text': 'Hello!' }
+    };
+
+    messagebird.conversations.start(params, function (err, response) {
+        if (err) {
+        return console.log(err);
+        }
+        console.log(response);
+      });
+})
+
+//Message Logs by Date
+
 
 app.listen(port, ()=> {
     console.log('app running on port ', port)
